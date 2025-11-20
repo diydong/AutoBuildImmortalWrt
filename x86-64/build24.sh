7f7384c94154d2b8cf08894fd6a0da6474315730
diydong <44688897+diydong@users.noreply.github.com>
2025-11-20 15:28:48 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -58,6 +58,10 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-proto-wireguard"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-app-ocserv"
+PACKAGES="$PACKAGES luci-app-wechatpush"
+
+
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
