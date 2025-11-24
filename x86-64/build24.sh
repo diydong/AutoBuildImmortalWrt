25493af62902cddc21ec942d0af250caf00843a3
diydong <44688897+diydong@users.noreply.github.com>
2025-11-24 14:22:45 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -57,7 +57,6 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-proto-wireguard"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ocserv-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-wechatpush-zh-cn"
 
